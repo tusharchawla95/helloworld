30b42ff963e0f7d62cd4ba9a8384ccaae239c241
tusharchawla95 <tusharchawla95@gmail.com>
2021-08-24 11:16:16 +0530

    Final Commit

--- src/main/java/com/example/HelloWorld/HelloWorldController.java
+++ src/main/java/com/example/HelloWorld/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/")
-    public String helloWorld()
+    public String HelloWorld()
     {
         return "Hello World";
     }
